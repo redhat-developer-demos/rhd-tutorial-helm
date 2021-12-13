9398536190bcc6a30e64e001aa56e6b623e72b13
ammbra <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>
2021-12-13 19:37:19 +0100

    two more queries added when initializing the project.

--- src/main/resources/import-prod.sql
+++ src/main/resources/import-prod.sql
@@ -7,7 +7,21 @@ values
 
 insert
 into
+    question
+(region, text, title, id)
+values
+    ('CEE', 'Why do we dream?', 'Existence', nextval('question_sequence'));
+
+insert
+into
     answer
 (text, question_id,  id)
 values
 ('Yes I am', currval('question_sequence') , nextval('answer_sequence'));
+
+insert
+into
+    answer
+(text, question_id,  id)
+values
+    ('To update our brains', currval('question_sequence') , nextval('answer_sequence'));
--- src/main/resources/import-test.sql
+++ src/main/resources/import-test.sql
@@ -3,11 +3,25 @@ into
     question
 (region, text, title, id)
 values
-('BeNeLux', 'Are you there?', 'Existence', nextval('question_sequence'));
+    ('BeNeLux', 'Are you there?', 'Existence', nextval('question_sequence'));
+
+insert
+into
+    question
+(region, text, title, id)
+values
+    ('cee', 'Why do we dream?', 'Existence', nextval('question_sequence'));
 
 insert
 into
     answer
 (text, question_id,  id)
 values
-('Yes I am', currval('question_sequence') , nextval('answer_sequence'));
+    ('Yes I am', currval('question_sequence') , nextval('answer_sequence'));
+
+insert
+into
+    answer
+(text, question_id,  id)
+values
+    ('To update our brains', currval('question_sequence') , nextval('answer_sequence'));
